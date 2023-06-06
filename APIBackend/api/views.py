e7c8bd752c0a8c62fca6c0926812aac8e5e2619e
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-06 16:39:33 +0300

    Mise en place de views

--- APIBackend/api/views.py
+++ APIBackend/api/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+
+from django.views.decorators.csrf import csrf_exempt
+
+from .serializers import ArticleSerializer
+from .models import Article
 
 # Create your views here.
-def Index(request):
-    return HttpResponse("It is working")
+@csrf_exempt
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        
+        return JsonResponse(serializer.errors, status=400)
