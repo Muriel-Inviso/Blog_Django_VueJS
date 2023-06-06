fe77e5e3f3a94f6a49f0374990fa8b33abd1c44d
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-06 16:51:53 +0300

    Ajout de article details et save 01:17:33

--- APIBackend/api/urls.py
+++ APIBackend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import article_list, article_details
 
 urlpatterns = [
-    path('articles/', views.article_list)
+    path('articles/', article_list),
+    path('articles/<int:pk>/', article_details)
 ]
--- APIBackend/api/views.py
+++ APIBackend/api/views.py
@@ -8,6 +8,8 @@ from .serializers import ArticleSerializer
 from .models import Article
 
 # Create your views here.
+
+
 @csrf_exempt
 def article_list(request):
     if request.method == 'GET':
@@ -22,5 +24,29 @@ def article_list(request):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
-        
+
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def article_details(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=204)
