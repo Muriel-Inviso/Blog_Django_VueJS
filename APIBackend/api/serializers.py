967ce78887f39561357e8734c02bf822e5fbfa03
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-06 16:25:11 +0300

    Rectification de serializers

--- APIBackend/api/serializers.py
+++ APIBackend/api/serializers.py
@@ -2,15 +2,7 @@ from rest_framework import serializers
 
 from . import models
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=400)
-
-    def create(self, validated_data):
-        return models.Article.objects.create(validated_data)
-    
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        # return models.Article.update(instance, validated_data)
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Article
+        fields = ['id', 'title', 'description']
